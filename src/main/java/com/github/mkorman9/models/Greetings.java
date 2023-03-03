daa81b914d8b2a61c3db7c0e2047c85d8882cf54
Michał Korman <michal.korman@icloud.com>
2023-03-03 20:17:17 +0100

    use record type for Greetings

--- src/main/java/com/github/mkorman9/models/Greetings.java
+++ src/main/java/com/github/mkorman9/models/Greetings.java
@@ -1,12 +1,6 @@
 package com.github.mkorman9.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Greetings {
-    private String text;
+public record Greetings(
+        String text
+) {
 }
