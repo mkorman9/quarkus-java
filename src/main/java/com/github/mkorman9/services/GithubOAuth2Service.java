2b6bb20b0e07cdc31fdfbb4755b75e4be731ec78
Michał Korman <michal.korman@icloud.com>
2023-03-05 23:07:45 +0100

    implement in-memory oauth2 state management

--- src/main/java/com/github/mkorman9/services/GithubOAuth2Service.java
+++ src/main/java/com/github/mkorman9/services/GithubOAuth2Service.java
@@ -2,6 +2,7 @@ package com.github.mkorman9.services;
 
 import com.github.scribejava.apis.GitHubApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.exceptions.OAuthException;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -9,33 +10,43 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 @ApplicationScoped
 public class GithubOAuth2Service {
     private final OAuth20Service service;
+    private final OAuth2StateService stateService;
 
     @Inject
     public GithubOAuth2Service(
             @ConfigProperty(name="oauth2.github.clientId") String clientId,
             @ConfigProperty(name="oauth2.github.clientSecret") String clientSecret,
-            @ConfigProperty(name="oauth2.github.redirectUrl") String redirectUrl
+            @ConfigProperty(name="oauth2.github.redirectUrl") String redirectUrl,
+            OAuth2StateService stateService
     ) {
         this.service = new ServiceBuilder(clientId)
                 .apiSecret(clientSecret)
                 .callback(redirectUrl)
                 .build(GitHubApi.instance());
+        this.stateService = stateService;
     }
 
     public String getAuthorizationUrl() {
-        return service.getAuthorizationUrl("state");
+        return service.getAuthorizationUrl(stateService.generateState());
     }
 
-    public OAuth2AccessToken resolveAccessToken(String code) {
+    public Optional<OAuth2AccessToken> resolveAccessToken(String code, String state) {
+        if (!stateService.validateState(state)) {
+            return Optional.empty();
+        }
+
         try {
-            return service.getAccessToken(code);
+            return Optional.of(service.getAccessToken(code));
         } catch (IOException | InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
+        } catch (OAuthException e) {
+            return Optional.empty();
         }
     }
 }
---
+++ src/main/java/com/github/mkorman9/services/OAuth2StateService.java
@@ -0,0 +1,21 @@
+package com.github.mkorman9.services;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ApplicationScoped
+public class OAuth2StateService {
+    private final Map<String, Boolean> store = new ConcurrentHashMap<>();
+
+    public String generateState() {
+        var state = UUID.randomUUID().toString();
+        store.put(state, true);
+        return state;
+    }
+
+    public boolean validateState(String state) {
+        return store.remove(state);
+    }
+}
