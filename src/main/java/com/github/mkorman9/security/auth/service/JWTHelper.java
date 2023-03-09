0251d0698f4b3953085e1546cfcb220c343cee79
Michał Korman <michal.korman@icloud.com>
2023-03-09 16:57:53 +0100

    lint

--- src/main/java/com/github/mkorman9/security/auth/service/JWTHelper.java
+++ src/main/java/com/github/mkorman9/security/auth/service/JWTHelper.java
@@ -5,16 +5,12 @@ import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Verification;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 @ApplicationScoped
 public class JWTHelper {
-    private static final Logger LOG = LoggerFactory.getLogger(JWTHelper.class);
-
     private final Algorithm algorithm;
 
     @Inject
