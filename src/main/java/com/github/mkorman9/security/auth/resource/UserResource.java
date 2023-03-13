21787cd6c4e85234270009f33f99082265bb8fcb
Michał Korman <michal.korman@icloud.com>
2023-03-13 22:56:13 +0100

    lint

--- src/main/java/com/github/mkorman9/security/auth/resource/UserResource.java
+++ src/main/java/com/github/mkorman9/security/auth/resource/UserResource.java
@@ -12,17 +12,16 @@ import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.util.List;
 import java.util.UUID;
 
 @Path("/user")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
     private static final Logger LOG = LoggerFactory.getLogger(UserResource.class);
 
