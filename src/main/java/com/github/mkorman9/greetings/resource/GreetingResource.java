cfa94783e6f9c2ca2a56f84e7d4166e6fd77e49d
Michał Korman <michal.korman@icloud.com>
2023-04-12 18:17:36 +0200

    Revert "convert GreetingResource to non-blocking code"
    This reverts commit 145185fb09670ce87cca912f47b18339598f58a5.

--- src/main/java/com/github/mkorman9/greetings/resource/GreetingResource.java
+++ src/main/java/com/github/mkorman9/greetings/resource/GreetingResource.java
@@ -2,7 +2,8 @@ package com.github.mkorman9.greetings.resource;
 
 import com.github.mkorman9.greetings.dto.Greetings;
 import com.github.mkorman9.greetings.service.GreetingService;
-import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -10,6 +11,7 @@ import javax.ws.rs.Path;
 
 @Path("/hello")
 public class GreetingResource {
+    private static final Logger LOG = LoggerFactory.getLogger(GreetingResource.class);
 
     private final GreetingService greetingService;
 
@@ -19,8 +21,8 @@ public class GreetingResource {
     }
 
     @GET
-    public Uni<Greetings> greet() {
-        var greetings = greetingService.generate();
-        return Uni.createFrom().item(greetings);
+    public Greetings greet() {
+        LOG.info("Greetings!");
+        return greetingService.generate();
     }
 }
