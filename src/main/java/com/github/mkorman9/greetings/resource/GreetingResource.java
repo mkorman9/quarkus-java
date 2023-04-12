26346d82186a257235887fcfecee339bafddab1e
Michał Korman <michal.korman@icloud.com>
2023-04-13 00:12:46 +0200

    remove logging from GreetingResource

--- src/main/java/com/github/mkorman9/greetings/resource/GreetingResource.java
+++ src/main/java/com/github/mkorman9/greetings/resource/GreetingResource.java
@@ -2,8 +2,6 @@ package com.github.mkorman9.greetings.resource;
 
 import com.github.mkorman9.greetings.dto.Greetings;
 import com.github.mkorman9.greetings.service.GreetingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -11,8 +9,6 @@ import javax.ws.rs.Path;
 
 @Path("/hello")
 public class GreetingResource {
-    private static final Logger LOG = LoggerFactory.getLogger(GreetingResource.class);
-
     private final GreetingService greetingService;
 
     @Inject
@@ -22,7 +18,6 @@ public class GreetingResource {
 
     @GET
     public Greetings greet() {
-        LOG.info("Greetings!");
         return greetingService.generate();
     }
 }
