9b2d793ef135ba83129c5f112c48c32c23a71b7f
Michał Korman <michal.korman@icloud.com>
2023-04-17 18:14:27 +0200

    rework the way of generating the secure token

--- src/main/java/com/github/mkorman9/security/auth/service/TokenService.java
+++ src/main/java/com/github/mkorman9/security/auth/service/TokenService.java
@@ -2,31 +2,28 @@ package com.github.mkorman9.security.auth.service;
 
 import com.github.mkorman9.security.auth.model.Token;
 import com.github.mkorman9.security.auth.model.User;
-import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.transaction.Transactional;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.Instant;
 import java.util.Optional;
 
 @ApplicationScoped
 public class TokenService {
+    private static final Logger LOG = LoggerFactory.getLogger(TokenService.class);
     private static final String TOKEN_CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final int TOKEN_LENGTH = 48;
+    private static final SecureRandom RANDOM = selectSecureRandom();
 
     @Inject
     EntityManager entityManager;
-
-    private final SecureRandom random;
-
-    @SneakyThrows
-    public TokenService() {
-        random = SecureRandom.getInstanceStrong();
-    }
 
     @Transactional
     public Optional<Token> findToken(String token) {
@@ -56,9 +53,22 @@ public class TokenService {
     }
 
     private String generateToken() {
-        return random.ints(TOKEN_LENGTH, 0, TOKEN_CHARSET.length())
+        return RANDOM.ints(TOKEN_LENGTH, 0, TOKEN_CHARSET.length())
                 .mapToObj(TOKEN_CHARSET::charAt)
                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
                 .toString();
+    }
+
+    private static SecureRandom selectSecureRandom() {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                return SecureRandom.getInstanceStrong();
+            } else {
+                return SecureRandom.getInstance("NativePRNGNonBlocking");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            LOG.error("SecureRandom algorithm cannot be selected", e);
+            throw new RuntimeException(e);
+        }
     }
 }
