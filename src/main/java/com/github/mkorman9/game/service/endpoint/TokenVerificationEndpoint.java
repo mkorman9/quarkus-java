d9e8eafa65047703ae98a6b044a1cb50d5dc62bc
Michal Korman <michal.korman@icloud.com>
2023-05-30 15:13:47 +0200

    refactor

--- src/main/java/com/github/mkorman9/game/service/endpoint/TokenVerificationEndpoint.java
+++ src/main/java/com/github/mkorman9/game/service/endpoint/TokenVerificationEndpoint.java
@@ -1,4 +1,4 @@
-package com.github.mkorman9.game.bus;
+package com.github.mkorman9.game.service.endpoint;
 
 import com.github.mkorman9.game.dto.TokenVerificationRequest;
 import com.github.mkorman9.game.dto.TokenVerificationResponse;
