c54533a5c6a400232aa9135e707eb49a38a3d983
Michal Korman <michal.korman@icloud.com>
2023-06-12 00:37:28 +0200

    fix sql schema

--- src/main/resources/db/migration/V1.0.0__base.sql
+++ src/main/resources/db/migration/V1.0.0__base.sql
@@ -8,7 +8,7 @@ CREATE TABLE user_roles (
     user_id UUID NOT NULL,
     role    TEXT NOT NULL,
 
-    CONSTRAINT pk_user_roles UNIQUE(user_id, role),
+    CONSTRAINT pk_user_roles PRIMARY KEY(user_id, role),
     CONSTRAINT fk_user_roles_users FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
